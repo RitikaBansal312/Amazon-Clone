564674617827a956a51b351220f7a160ae167bc3
Ritika Bansal <ritikabansal5678@gmail.com>
2022-02-27 20:12:01 +0530

    day 2 build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
 function App() {
-    return <div className="app">React App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/home" component={Home} />
+          <Route path="/header" component={Header} />
+        </Routes>
+      </Router>
+      <Header />
+      <Home />
+    </div>
+  );
 }
 export default App;
